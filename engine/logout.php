9c40a55359fc060b0707e8c95c3a8e560611142d
Lucas Anderson <135335242+lucande2@users.noreply.github.com>
2023-07-12 19:20:52 +0200

    Update logout.php
    QA Code review

--- engine/logout.php
+++ engine/logout.php
@@ -1,9 +1,15 @@
 <?php
+/*
+    engine/logout.php    VERSION 1.3
+    Initiates the logout process.
+    Reviewed 7/12/2023
+*/
+
 session_start();
 // Unset all of the session variables
 $_SESSION = array();
-// If it's desired to kill the session, also delete the session cookie.
-// Note: This will destroy the session, and not just the session data!
+
+// This will destroy the session, and not just the session data!
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -11,6 +17,7 @@ if (ini_get("session.use_cookies")) {
         $params["secure"], $params["httponly"]
     );
 }
+
 // Finally, destroy the session.
 session_destroy();
 header("Location: https://project.lucande.io/index.php");
